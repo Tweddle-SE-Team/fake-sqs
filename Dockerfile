f00a458082cc005e3f00f5ab27c3fcc9cf574d53
Andrii Chubatiuk <a.chubatiuk@cogniance.com>
2017-09-20 10:08:58 +0300

    attributes md5 test

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,11 @@ ENV GOPATH=/opt
 COPY . $GOPATH/src/github.com/Tweddle-SE-Team/goaws
 WORKDIR $GOPATH/src/github.com/Tweddle-SE-Team/goaws
 
-RUN go get ./... && go test ./... && go build -o /usr/bin/goaws .
+RUN go get -u github.com/aws/aws-sdk-go/... && \
+    go get github.com/stretchr/testify && \
+    go get ./... && \
+    go test ./... && \
+    go build -o /usr/bin/goaws .
 
 COPY config/config.yaml /etc/goaws/
 
--- goaws.go
+++ goaws.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 
@@ -51,4 +54,58 @@ func main() {
 	} else {
 		log.Fatal("Not enough or too many ports defined to start GoAws.")
 	}
+}
+
+type Server struct {
+	closed   bool
+	handler  http.Handler
+	listener net.Listener
+	mu       sync.Mutex
+}
+
+// Quit closes down the server.
+func (srv *Server) Quit() error {
+	srv.mu.Lock()
+	srv.closed = true
+	srv.mu.Unlock()
+
+	return srv.listener.Close()
+}
+
+// URL returns a URL for the server.
+func (srv *Server) URL() string {
+	return "http://" + srv.listener.Addr().String()
+}
+
+// New starts a new server and returns it.
+func New(addr string) (*Server, error) {
+	if addr == "" {
+		addr = "localhost:0"
+	}
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot listen on localhost: %v", err)
+	}
+
+	srv := Server{listener: l, handler: router.New()}
+
+	go http.Serve(l, &srv)
+
+	return &srv, nil
+}
+
+func (srv *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	srv.mu.Lock()
+	closed := srv.closed
+	srv.mu.Unlock()
+
+	if closed {
+		hj := w.(http.Hijacker)
+		conn, _, _ := hj.Hijack()
+		conn.Close()
+		return
+	}
+
+	srv.handler.ServeHTTP(w, req)
 }
